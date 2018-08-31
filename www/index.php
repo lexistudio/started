57c31f156a0f374c18bd97323e8d80a95432483c
Alexander Krylov <akrylov@elar.ru>
2018-08-31 12:34:16 +0300

    started projects

---
+++ www/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1.0,">
+  <title>stylus</title>
+  <link rel="stylesheet" href="/css/site.css">
+</head>
+<body>
+  
+
+  <script data-main="/js/app/site.js" src="/js/require-jquery.js"></script>
+</body>
+</html>
